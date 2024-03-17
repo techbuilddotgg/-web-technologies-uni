2709f69bd8c7ef17cadf9d19c7b22816b543d563
perkzen <perko.domen@gmail.com>
2024-03-17 20:22:30 +0100

    feat: finish

--- src/server.js
+++ src/server.js
@@ -4,16 +4,49 @@ import * as fs from "fs";
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
-    fs.readFile("public/index.html", (err, data) => {
-        if (err) {
-            res.writeHead(500, {"Content-Type": "text/plain"});
-            res.end("Failed to load HTML file.");
-            return;
-        }
 
-        res.writeHead(200, {"Content-Type": "text/html"});
-        res.end(data);
-    });
+    const requestUrl = req.url;
+
+    if (requestUrl === "/") {
+        fs.readFile("public/podatkovni-model/index.html", (err, data) => {
+            if (err) {
+                res.writeHead(500, {"Content-Type": "text/plain"});
+                res.end("Failed to load HTML file.");
+                return;
+            }
+
+            res.writeHead(200, {"Content-Type": "text/html"});
+            res.end(data);
+        });
+    }
+
+    if (requestUrl === "/er.png") {
+        fs.readFile("public/podatkovni-model/er.png", (err, data) => {
+            if (err) {
+                res.writeHead(500, {"Content-Type": "text/plain"});
+                res.end("Failed to load image file.");
+                return;
+            }
+
+            res.writeHead(200, {"Content-Type": "image/png"});
+            res.end(data);
+        });
+    }
+
+    if (requestUrl === "/rest") {
+        fs.readFile("public/rest/index.html", (err, data) => {
+            if (err) {
+                res.writeHead(500, {"Content-Type": "text/plain"});
+                res.end("Failed to load HTML file.");
+                return;
+            }
+
+            res.writeHead(200, {"Content-Type": "text/html"});
+            res.end(data);
+        });
+    }
+
+
 });
 server.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}/`);
